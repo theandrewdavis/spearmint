dae0164f1f79efc448ffb918b759c1cfa058de33
Andrew Davis <theandrewdavis@gmail.com>
2015-01-07 08:39:26 -0500

    Create account and transaction classes

--- server.py
+++ server.py
@@ -3,20 +3,40 @@ import sqlite3
 import yaml
 from apscheduler.schedulers.background import BackgroundScheduler
 
-# Bank details for fetching transactions from web
-class BankConfig(object):
+# Info needed to fetch all accounts from a bank
+class BankInfo(object):
     def __init__(self, **kwargs):
         self.bank = kwargs['bank']
         self.username = kwargs['username']
         self.password = kwargs['password']
         self.account_names = kwargs.get('account_names', {})
 
-    # Create array of BankConfigs from yaml file
+    # Create array of BankInfos from yaml file
     @staticmethod
     def from_file(filename):
-        bank_configs = []
         with open(filename, 'r') as file:
-            return [BankConfig(**config) for config in yaml.load(file.read())]
+            return [BankInfo(**info) for info in yaml.load(file.read())]
+
+class Account(object):
+    def __init__(self, bank_info):
+        self.bank_info = bank_info
+        self.balance = '0.00'
+        self.transactions = []
+
+class Transaction(object):
+    @staticmethod
+    def from_tuple(tx_tuple):
+        transaction = Transaction()
+        transaction.date = tx_tuple[0]
+        transaction.amount = tx_tuple[1]
+        transaction.from_account = tx_tuple[2]
+        transaction.to_account = tx_tuple[3]
+        transaction.description = tx_tuple[4]
+        return transaction
+
+    def to_tuple(self):
+        return (self.date, self.amount, self.from_account, self.to_account, self.description)
+
 
 # Transactions as JSON
 @bottle.route('/api/transactions')
@@ -24,9 +44,7 @@ def api_transactions():
     connection = sqlite3.connect('db.sqlite3')
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM transactions')
-    transactions = []
-    for row in cursor.fetchall():
-        transactions.append({'date': row[0], 'amount': row[1], 'from': row[2], 'to': row[3], 'description': row[4]})
+    transactions = [Transaction.from_tuple(row) for row in cursor.fetchall()]
     connection.close()
     return {'transactions': transactions}
 
@@ -41,19 +59,21 @@ def static(path):
     return bottle.static_file(path, root='static')
 
 # Fetch transactions from web
-def fetch(bank_config):
-    return [
-        {'date': '1/2/2015', 'amount': '1,000.00', 'from': 'Income:Salary', 'to': 'A:Checking', 'description': 'Salary'},
-        {'date': '1/2/2015', 'amount': '1.00', 'from': 'A:Cash', 'to': 'E:Food', 'description': 'Soda'}
+def fetch(bank_info):
+    account = Account(bank_info)
+    account.transactions = [
+        Transaction.from_tuple(('1/2/2015', '1,000.00', 'Income:Salary', 'A:Checking', 'Salary')),
+        Transaction.from_tuple(('1/2/2015', '1.00', 'A:Cash', 'E:Food', 'Soda'))
     ]
+    return [account]
 
 # Add transactions from web to database
 def merge():
-    bank_configs = BankConfig.from_file('banks.yaml')
     transactions = []
-    for bank_config in bank_configs:
-        for transaction in fetch(bank_config):
-            transactions.append((transaction['date'], transaction['amount'], transaction['from'], transaction['to'], transaction['description']))
+    for bank in BankInfo.from_file('banks.yaml'):
+        for account in fetch(bank):
+            for transaction in account.transactions:
+                transactions.append(transaction.to_tuple())
     connection = sqlite3.connect('db.sqlite3')
     cursor = connection.cursor()
     cursor.executemany('INSERT INTO transactions VALUES (?,?,?,?,?)', transactions)
