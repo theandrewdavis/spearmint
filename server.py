8551e62c00f2a6b53b5ce3ae7a3261ecd46e29e2
Andrew Davis <theandrewdavis@gmail.com>
2015-01-06 21:40:01 -0500

    Make server load bank info from file

--- server.py
+++ server.py
@@ -1,6 +1,22 @@
 import bottle
 import sqlite3
+import yaml
 from apscheduler.schedulers.background import BackgroundScheduler
+
+# Bank details for fetching transactions from web
+class BankConfig(object):
+    def __init__(self, **kwargs):
+        self.bank = kwargs['bank']
+        self.username = kwargs['username']
+        self.password = kwargs['password']
+        self.account_names = kwargs.get('account_names', {})
+
+    # Create array of BankConfigs from yaml file
+    @staticmethod
+    def from_file(filename):
+        bank_configs = []
+        with open(filename, 'r') as file:
+            return [BankConfig(**config) for config in yaml.load(file.read())]
 
 # Transactions as JSON
 @bottle.route('/api/transactions')
@@ -25,7 +41,7 @@ def static(path):
     return bottle.static_file(path, root='static')
 
 # Fetch transactions from web
-def fetch():
+def fetch(bank_config):
     return [
         {'date': '1/2/2015', 'amount': '1,000.00', 'from': 'Income:Salary', 'to': 'A:Checking', 'description': 'Salary'},
         {'date': '1/2/2015', 'amount': '1.00', 'from': 'A:Cash', 'to': 'E:Food', 'description': 'Soda'}
@@ -33,9 +49,11 @@ def fetch():
 
 # Add transactions from web to database
 def merge():
+    bank_configs = BankConfig.from_file('banks.yaml')
     transactions = []
-    for transaction in fetch():
-        transactions.append((transaction['date'], transaction['amount'], transaction['from'], transaction['to'], transaction['description']))
+    for bank_config in bank_configs:
+        for transaction in fetch(bank_config):
+            transactions.append((transaction['date'], transaction['amount'], transaction['from'], transaction['to'], transaction['description']))
     connection = sqlite3.connect('db.sqlite3')
     cursor = connection.cursor()
     cursor.executemany('INSERT INTO transactions VALUES (?,?,?,?,?)', transactions)
